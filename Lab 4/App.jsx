d36772b6807007182995778a7d5e614f669bbac7
deepakdnayak <deepakdnayak2004@gmail.com>
2025-04-14 12:12:58 +0530

    Added Lab 4

---
+++ Lab 4/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ToDoFunction from './components/ToDoFunction'
+
+const App = () => {
+  return (
+    <div>
+      <ToDoFunction />
+    </div>
+  )
+}
+
+export default App
